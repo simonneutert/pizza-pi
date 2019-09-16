708f16ec1371a1c0bf4b04f06250fc58b7488dd1
Simon Neutert <Simon.Neutert@gmail.com>
2019-09-16 16:57:49 +0200

    vuelidations

--- src/components/PizzaCalculatorForm.vue
+++ src/components/PizzaCalculatorForm.vue
@@ -9,6 +9,12 @@
             v-model="pizza.diameter"
             placeholder="diameter in cm"
           />
+          <div class="error" v-if="!$v.pizza.diameter.required">
+            Field is required
+          </div>
+          <div class="error" v-if="!$v.pizza.diameter.between">
+            Price cannot be negative or higher than 100
+          </div>
         </div>
       </div>
       <div class="col-md-6">
@@ -19,6 +25,12 @@
             v-model="pizza.price"
             placeholder="price in Euro"
           />
+          <div class="error" v-if="!$v.pizza.price.required">
+            Field is required
+          </div>
+          <div class="error" v-if="!$v.pizza.price.between">
+            Price cannot be negative or higher than 100
+          </div>
         </div>
       </div>
     </div>
@@ -26,8 +38,22 @@
 </template>
 
 <script>
+import { required, between } from "vuelidate/lib/validators";
+
 export default {
   name: "pizza-calculator-form",
-  props: ["pizza"]
+  props: ["pizza"],
+  validations: {
+    pizza: {
+      price: {
+        required,
+        between: between(0, 100)
+      },
+      diameter: {
+        required,
+        between: between(0, 100)
+      }
+    }
+  }
 };
 </script>
